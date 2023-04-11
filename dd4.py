b8e093a17f3cc85955d3bbdd1596bb5224879262
unknown <dbnkds91@gmail.com>
2023-04-11 18:14:38 +0530

    first commit

---
+++ dd4.py
@@ -0,0 +1,52 @@
+import cv2
+
+# Load the pre-trained eye and face cascade classifiers
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Create a video capture object
+cap = cv2.VideoCapture(0)
+
+# Loop over frames from the video capture
+while True:
+    # Read a frame from the video capture
+    ret, frame = cap.read()
+    
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    
+    # Loop over the detected faces
+    for (x,y,w,h) in faces:
+        # Draw a rectangle around the face
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+        
+        # Crop the face region from the grayscale frame
+        roi_gray = gray[y:y+h, x:x+w]
+        
+        # Detect eyes in the face region
+        eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=5)
+        
+        # Loop over the detected eyes
+        for (ex,ey,ew,eh) in eyes:
+            # Draw a rectangle around the eye
+            cv2.rectangle(frame, (x+ex,y+ey), (x+ex+ew,y+ey+eh), (0,255,0), 2)
+            
+            # Check if eye glasses are detected
+            if ew > 60 and eh > 30:
+                cv2.putText(frame, "Eye glasses detected", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
+            else:
+                cv2.putText(frame, "No eye glasses detected", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,255), 2)
+            
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    
+    # Check for key presses
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
